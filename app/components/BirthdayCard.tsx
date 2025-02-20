7683eddc11ff7a18171523be64483cca5dcc9c67
Ayush Singh <ayush1009208@gmail.com>
2025-02-21 05:07:59 +0530

    Sab done

--- app/components/BirthdayCard.tsx
+++ app/components/BirthdayCard.tsx
@@ -542,7 +542,7 @@ const BirthdayCard = () => {
                     className="mt-6 text-3xl font-bold text-white"
                   >
                     {giftUnwrapped ? (
-                      "A special surprise awaits you! Check your email 📧"
+                      "Chall hat badi ayi pagaliya surprise legi"
                     ) : (
                       "Click to unwrap your gift!"
                     )}
